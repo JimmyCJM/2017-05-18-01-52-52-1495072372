24926c2bdedb6115b882d7efcec6a2051bbe9327
JimmyCJM <chijieming@sina.com>
2017-07-05 15:04:34 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,8 +1,6 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log('o) 最小值 = '+sequence.minimum()+
-		'\no)最大值 = '+sequence.maxmu()+
-		'\no) 元素个数 = '+sequence.len+
+    console.log('o) 最小值 = '+sequence.minimum()+'\no)最大值 = '+sequence.maxmu()+'\no) 元素数量 = '+sequence.len+
 		'\no) 平均值 = '+sequence.average());
 };
 
